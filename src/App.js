a24a472890a618e14f71bca9c882803997d16d3f
W2_63112_Tungavidya <vidyasarkar96@gmail.com>
2023-05-01 21:19:56 +0530

    nn,m

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Navbar from "./component/navbar";
-import Login from "./user/login";
+//import Login from "./user/login";
+import Login from "./user/login_redux";
 import Signup from "./user/signup";
 import Home from "./user/home";
 import UserDetails from "./user/userDetails";
@@ -14,6 +15,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Login />} />
+        {/* <Route path="/" element={<Login />} /> */}
         <Route path="/signup" element={<Signup />} />
         <Route path="/home" element={<Home />} />
         <Route path="/userDetails" element={<UserDetails />} />
